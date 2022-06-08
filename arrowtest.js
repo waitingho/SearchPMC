e6a61ce1ea564e1b6cdfdf6c139ed30827161131
waitingho <waitingho4@gmail.com>
2022-06-08 17:39:12 +0800

    Update arrowtest.js

--- arrowtest.js
+++ arrowtest.js
@@ -9,6 +9,26 @@ const { resolve } = require('path');
 const data = [];
 const https = require('https');
 const lcscsearch = async (p) => {
+  
+  axios.get('/user/12345')
+  .catch(function (error) {
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(error.response.headers);
+    } else if (error.request) {
+      // The request was made but no response was received
+      // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+      // http.ClientRequest in node.js
+      console.log(error.request);
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.log('Error', error.message);
+    }
+    console.log(error.config);
+  });
 
   var config = {
     method: 'get',
@@ -56,12 +76,12 @@ app.get('/arrow', async (req, res) => {
     let ress = await Promise.all(promises);
     console.log('幹你娘成功ㄌ')
     
-console.log(JSON.stringify(req.headers))
+// console.log(JSON.stringify(req.headers))
     res.send(data);
   } catch (err) {
     console.log(err);
     
-console.log(JSON.stringify(req.headers))
+// console.log(JSON.stringify(req.headers))
     res.send('哈哈白癡又失敗ㄌ');
    
   }
