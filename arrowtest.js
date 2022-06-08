050875712b357bc11b06cb87758a457bc62aa906
waitingho <waitingho4@gmail.com>
2022-06-08 09:07:09 +0800

    Update arrowtest.js

--- arrowtest.js
+++ arrowtest.js
@@ -13,9 +13,9 @@ const lcscsearch = async (p) => {
   var config = {
     method: 'get',
     url: `https://www.arrow.com/zh-cn/components-services/why-components-services`,
-//     timeout: 60000, //optional
-//     maxContentLength: 10000000,
-//     httpsAgent: new https.Agent({ keepAlive: false }),
+    timeout: 60000, //optional
+    maxContentLength: 10000000,
+    httpsAgent: new https.Agent({ keepAlive: false }),
   };
 
    
