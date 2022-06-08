943996ba1e5272a34aac3bdf146c58438b0c8118
waitingho <waitingho4@gmail.com>
2022-06-08 17:57:48 +0800

    Update arrowtest.js

--- arrowtest.js
+++ arrowtest.js
@@ -10,25 +10,7 @@ const data = [];
 const https = require('https');
 const lcscsearch = async (p) => {
   
-  axios.get('/user/12345')
-  .catch(function (error) {
-    if (error.response) {
-      // The request was made and the server responded with a status code
-      // that falls out of the range of 2xx
-      console.log(error.response.data);
-      console.log(error.response.status);
-      console.log(error.response.headers);
-    } else if (error.request) {
-      // The request was made but no response was received
-      // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-      // http.ClientRequest in node.js
-      console.log(error.request);
-    } else {
-      // Something happened in setting up the request that triggered an Error
-      console.log('Error', error.message);
-    }
-    console.log(error.config);
-  });
+
 
   var config = {
     method: 'get',
@@ -40,6 +22,25 @@ const lcscsearch = async (p) => {
 
    
   try {
+    // Add a request interceptor
+axios.interceptors.request.use(function (config) {
+    // Do something before request is sent
+    return config;
+  }, function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+  });
+
+// Add a response interceptor
+axios.interceptors.response.use(function (response) {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    return response;
+  }, function (error) {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    return Promise.reject(error);
+  });
     let response = await axios(config);
     const $ = cheerio.load(response.data);
 
