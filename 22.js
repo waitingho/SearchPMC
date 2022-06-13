88fc63a054de1bd6cb61adf6312ed617710ef76c
waitingho <waitingho4@gmail.com>
2022-06-13 10:32:52 +0800

    Update 22.js

--- 22.js
+++ 22.js
@@ -1,59 +1,83 @@
 
 
 
+
 const puppeteer = require('puppeteer');
+const express = require('express');
+const app = express();
 const cheerio = require('cheerio');
-
-
+const port = 3001
+const { resolve } = require('path');
+const data = []
 
 const mesearch = async (p) => {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({ headless: true, executablePath: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe' });
     const page = await browser.newPage();
-    //ti url
+    //mouser url
     await page.goto(`https://www.mouser.tw/ProductDetail/Texas-Instruments/${p}`);
     // await page.waitForSelector('body')
-    
-    let body = await page.content()
-   
-    let $ = await cheerio.load(body)
-    
-    //先再外面建立陣列
-    let data = []
-    //我們把cheerio找到的資料轉成文字並存進data這個變數
-    await $('body').each((i, el) => {
-        let $1 = cheerio.load($(el).html())
-        //裏面建立一個物件並把資料都放進去
-        //這是物件 不了解的可以搜尋了解一下
-        let tmp = {
-            PartNumber: $1('#pdpProdInfo > div.panel-heading.pdp-product-card-header').text().trim(),
-            Inventory: $1('#pdpPricingAvailability > div.panel-body > div.row.pdp-product-availability > dl > dd:nth-child(2) > div.col-xs-3.onOrderQuantity').text().trim().replace(/\s+/g, ' '),
-            OnOrder: $1('#content-onOrderShipments').text().trim().replace(/\s+/g,' ').replace(/檢視預期日期/g,'').replace(/隱藏日期/g,'').split(' '),
-            pricing: $1('#pdpPricingAvailability > div.panel-body > div.pdp-pricing-table > table').text().trim().replace(/\s+/g, ' ').replace(' 零卷 / MouseReel™ †', '').slice(0,3).slice(4,6),
-        }
-        data.push(tmp)
-    })
-    
-    console.log(data);
+    try {
+        let body = await page.content()
+        let $ = await cheerio.load(body)
 
-    const fs = require('fs');
-    const content = JSON.stringify(data); //轉換成json格式
-    fs.appendFile(`infome.json`, content, 'utf8', function (err) {
-        if (err) {
-            return console.log(err);
-        }
-        console.log("The file was saved!");
-    });
-    
-    await browser.close()
-    
-}
-(function() {
-    const po = ['MSP430FR2633IRHBR?qs=VymPLiRQZITRQFkH8VS6GQ%3D%3D','CC2642R1FRGZR?qs=rrS6PyfT74fHZyxeWJx9DQ%3D%3D','TPS62050DGSR?qs=Gse6rAGbi7%252Bae8YoL367mQ%3D%3D','TPS62160DGKR?qs=2UFnnMkojnUsiNxEDO710Q%3D%3D'];
-    for (let p of po) {
-        mesearch(p);
+        //先再外面建立陣列
         
+        //我們把cheerio找到的資料轉成文字並存進data這個變數
+        await $('body').each((i, el) => {
+            let $1 = cheerio.load($(el).html())
+            //裏面建立一個物件並把資料都放進去
+            //這是物件 不了解的可以搜尋了解一下
+
+            let tmp = {
+                PartNumber: $1('#pdpProdInfo > div.panel-heading.pdp-product-card-header').text().trim(),
+                Inventory: $1('#pdpPricingAvailability > div.panel-body > div.row.pdp-product-availability > dl > dd:nth-child(2) > div.col-xs-3.onOrderQuantity').text().trim().replace(/\s+/g, ' '),
+                OnOrder: $1('#content-onOrderShipments').text().trim().replace(/\s+/g, ' ').replace(/檢視預期日期/g, '').replace(/隱藏日期/g, '').split(' '),
+                pricing: $1('#pdpPricingAvailability > div.panel-body > div.pdp-pricing-table > table').text().trim().replace(/\s+/g, ' ').replace(' 零卷 / MouseReel™ †', '').slice(0, 3).slice(4, 6),
+            }
+
+            data.push(tmp)
+        })
+
+        // console.log("data爬到ㄌ");
+
+        const fs = require('fs');
+        const content = JSON.stringify(data); //轉換成json格式
+        // await browser.close()
+        return await new Promise((resolve, reject) => {
+            fs.writeFile("infoti.json", content, 'utf8', function (err) {
+                if (err) reject(err);
+                
+                else resolve();
+            });
+        });
+    } catch (err) { throw err; }
+}
+
+console.log('幹你123娘');
+app.get('/', async (req, res) => {
+    let promises = [];
+    try {
+        const po = ['MSP430FR2633IRHBR?qs=VymPLiRQZITRQFkH8VS6GQ%3D%3D','CC2642R1FRGZR?qs=rrS6PyfT74fHZyxeWJx9DQ%3D%3D','TPS62050DGSR?qs=Gse6rAGbi7%252Bae8YoL367mQ%3D%3D','TPS62160DGKR?qs=2UFnnMkojnUsiNxEDO710Q%3D%3D'];
+        // const po = ['MSP430FR2633IRHBR?qs=VymPLiRQZITRQFkH8VS6GQ%3D%3D']
+        for (let p of po) {
+            promises.push(new Promise(async (resolve, reject) => {
+                try { await mesearch(p); resolve(); } catch (err2) { reject(err2); }
+            }));
+        }
+        let ress = await Promise.all(promises);
+        res.send(data);
+        console.log('幹你娘成功ㄌ');
+    } catch (err) {
+        res.send('白癡又失敗ㄌ');
+        console.log('白癡又失敗ㄌ');
     }
-   
-})();
+})
+
+// start and listen on the Express server
+app.listen(port, () => {
+    console.log(`Express is running on http://localhost:${port}`)
+})
+
+
 
 
