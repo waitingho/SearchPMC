41f09ef0aee5b0e9c367c7dd50837d6c87d4d2c9
waitingho <waitingho4@gmail.com>
2022-06-13 10:55:21 +0800

    Update mouser.js

--- mouser.js
+++ mouser.js
@@ -75,7 +75,7 @@ app.get('/mouser', async (req, res) => {
 
 // start and listen on the Express server
 app.listen(port, () => {
-    console.log(`Express is running on http://localhost:${port}`)
+    console.log(`Express is running on http://localhost:${port}/mouser`)
 })
 
 
