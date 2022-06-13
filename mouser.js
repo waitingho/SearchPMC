fa7fc329db07ce468f1bad3f5e3571ac39d0e1fc
waitingho <waitingho4@gmail.com>
2022-06-13 11:36:08 +0800

    Update mouser.js

--- mouser.js
+++ mouser.js
@@ -31,7 +31,7 @@ const mesearch = async (p) => {
             let tmp = {
                 partnumber: $1('#pdpProdInfo > div.panel-heading.pdp-product-card-header').text().trim(),
                 inventory: $1('#pdpPricingAvailability > div.panel-body > div.row.pdp-product-availability > dl > dd:nth-child(2) > div.col-xs-3.onOrderQuantity').text().trim().replace(/\s+/g, ' '),
-                onOrder: $1('#content-onOrderShipments').text().trim().replace(/\s+/g, ' ').replace(/檢視預期日期/g, '').replace(/隱藏日期/g, '').split(' '),
+                onOrder: $1('#content-onOrderShipments').text().trim(),
                 price: $1('#pdpPricingAvailability > div.panel-body > div.pdp-pricing-table > table').text().trim().replace(/\s+/g, ' ').replace(' 零卷 / MouseReel™ †', '').slice(0, 3).slice(4, 6),
             }
 
